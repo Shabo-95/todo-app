2bb0f489f8af59139e37bc246f961e40c90ad85c
Shabo-95 <Shabo-95@hotmail.com>
2022-07-20 12:22:26 +0200

    Create simple table for listing the todos

--- todoappclient/src/App.js
+++ todoappclient/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row min-vh-100">
+        <div className="col d-flex flex-column justify-content-center align-items-center">
+          <h1>ASP.NET & React ToDo App</h1>
+        </div>
+        {renderTodoItems()}
+      </div>
     </div>
   );
-}
 
-export default App;
+  function renderTodoItems() {
+    return (
+      <div className="table-responsive">
+        <table className="table table-bordered table-hover">
+          <thead>
+            <tr>
+              <th scope="col">State</th>
+              <th scope="col">Todos</th>
+              <th scope="col">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td scope="row">Finished</td>
+              <td scope="row">Todo Item 1</td>
+              <td scope="row">
+                <button type="button" class="btn btn-danger">
+                  Delete
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
